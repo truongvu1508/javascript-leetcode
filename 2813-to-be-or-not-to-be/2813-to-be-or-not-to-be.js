d4794a53f80a474bdd2527fd474f184c83c05c4f
Nguyễn Trường Vũ <163712038+truongvu1508@users.noreply.github.com>
2025-07-01 16:01:09 +0700

    Time: 50 ms (11.55%), Space: 52.8 MB (88.05%) - LeetHub

---
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(val1){
+            if(val !== val1){
+                throw new Error("Not Equal")
+            } else{
+                return true
+            }
+        },
+        notToBe: function(val2){
+            if(val === val2){
+                throw new Error("Equal")
+            }else{
+                return true
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
